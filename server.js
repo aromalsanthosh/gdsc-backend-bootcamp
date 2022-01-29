f79a65d47054119d55f808ba6e4d520831972202
Aromal S <aromalsanthosh@protonmail.com>
2022-01-29 20:01:08 +0530

    heroku deployment changes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars');
 
 const routes = require('./routes/taskRoutes');
 
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,6 +13,6 @@ app.set('view engine', 'hbs');
 
 app.use('/',routes);
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server started on port 3000');
 });
